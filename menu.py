57e91631abf83d006075302d4acc4ee158624021
datjandra <donny.tjandra@gmail.com>
2024-10-08 15:41:11 -0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,4 +3,4 @@ import streamlit as st
 def menu():
   st.sidebar.page_link("app.py", label="💪 Exercise Recommender")
   st.sidebar.page_link("pages/feedback.py", label="🖼️ Posture Analysis")
-  st.sidebar.page_link("pages/grading.py", label="📺 Motion Analysis")
+  st.sidebar.page_link("pages/grading.py", label="📺 Video Analysis")

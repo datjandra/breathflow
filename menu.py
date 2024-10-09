942025fba892a5fe55cfedc4b29bfea6c5eb6a6a
datjandra <donny.tjandra@gmail.com>
2024-10-08 17:10:21 -0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 def menu():
   st.set_page_config(page_title="Breathflow", page_icon='🕊️')
-  st.sidebar.page_link("app.py", label="💪 Exercise Recommender")
+  st.sidebar.page_link("app.py", label="💪 Exercise Recommendations")
   st.sidebar.page_link("pages/feedback.py", label="🖼️ Posture Analysis")
   st.sidebar.page_link("pages/grading.py", label="📺 Video Analysis")
